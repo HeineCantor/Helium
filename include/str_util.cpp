b7df9815eefa6e2eb09643d4736d0b6681131109
SgtSteel <ivanomatrisciano@gmail.com>
2018-10-10 01:49:31 +0200

    Add is_numeric(string)

--- include/str_util.cpp
+++ include/str_util.cpp
@@ -8,3 +8,15 @@ unsigned int str_len(string str)
 	//-1 perché c'è il post-incremento che sfancula tutto
 	return i-1;
 }
+
+bool is_numeric(string s)
+{
+	register unsigned int i = 0;
+	char c;
+	
+	while(c = s[i])
+		if (c < 48 || c > 57)
+			return false;
+	
+	return true;
+}
--- include/str_util.h
+++ include/str_util.h
@@ -1,3 +1,4 @@
 typedef const char* string;
 
 unsigned int str_len(string str);
+bool is_numeric(string s); // restituisce TRUE solo se la stringa è composta da soli numeri. Restituisce FALSE anche se contiene white-spaces, oppure se è espressa nel formato 0x1234
